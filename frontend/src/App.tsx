6357797a7d4029a932ee9ece82c65a89e6e6c85c
Ron Barrantes <ronbarrantes@gmail.com>
2025-06-05 19:14:49 -0400

    Add a test button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <>
       <div className="bg-green-300">Hello</div>
+      <Button>Click me</Button>
     </>
   );
 }
