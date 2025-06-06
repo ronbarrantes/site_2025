eb9a44c6f1b184b43a81ec21b2a52eaf381bcd4d
Ron Barrantes <ronbarrantes@gmail.com>
2025-06-05 20:28:09 -0400

    Add theme provider and mode toggle

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
-import { Button } from "./components/ui/button";
+import { ThemeProvider } from "@/components/theme-provider/theme-provider-provider";
+import { Button } from "@/components/ui/button";
+import { ModeToggle } from "./components/mode-toggle";
 
 function App() {
   return (
-    <>
+    <ThemeProvider>
+      <ModeToggle />
       <div className="bg-green-300">Hello</div>
       <Button>Click me</Button>
-    </>
+    </ThemeProvider>
   );
 }
 
