97e8b64e97d58ba7efc09f6ef4623919696db3c2
Ron Barrantes <ronbarrantes@gmail.com>
2025-06-05 22:43:37 -0400

    Did the linting

--- frontend/src/components/mode-toggle.tsx
+++ frontend/src/components/mode-toggle.tsx
@@ -1,5 +1,6 @@
 import { Moon, Sun } from "lucide-react";
 
+import { useTheme } from "@/components/theme-provider/theme-provider-state";
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
@@ -7,7 +8,6 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { useTheme } from "@/components/theme-provider/theme-provider-state";
 
 export function ModeToggle() {
   const { setTheme } = useTheme();
