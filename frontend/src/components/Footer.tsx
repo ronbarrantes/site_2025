8c72dec2335590438039e2de25ae15591b33a622
Ron Barrantes <ronbarrantes@gmail.com>
2025-06-07 15:40:34 -0400

    Add more stuff

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,92 +1,12 @@
-import { useState } from "react";
-
 import classNames from "classnames";
-import { NavLink } from "react-router";
-
-import { Logo } from "./logo/Logo";
-import { ModeToggle } from "./mode-toggle";
-
-const LinkItem = ({
-  children,
-  className,
-  to,
-}: {
-  to: string;
-  className?: string;
-  children: React.ReactNode;
-}) => {
-  return (
-    <li className="group p-1">
-      <NavLink
-        to={to}
-        className={classNames(
-          "border-black transition-all group-hover:border-b-4",
-          className
-        )}
-      >
-        {children}
-      </NavLink>
-    </li>
-  );
-};
-
-const logoColors = [
-  "group-hover:fill-slate-500 dark:group-hover:fill-slate-500",
-  "group-hover:fill-green-500 dark:group-hover:fill-green-500",
-  "group-hover:fill-cyan-500 dark:group-hover:fill-cyan-500",
-  "group-hover:fill-fuchsia-500 dark:group-hover:fill-fuchsia-500",
-  "group-hover:fill-lime-500 dark:group-hover:fill-lime-500",
-];
 
 export const Footer = ({ className }: { className: string }) => {
-  const [logoColorIdx, setLogoColorIdx] = useState<number>(3);
-
   return (
     <div className={classNames(className)}>
-      <header className="glass m-2 flex items-center justify-between rounded-xl p-1 drop-shadow-xl">
-        <span>
-          <NavLink
-            onMouseOver={() => {
-              setLogoColorIdx(
-                logoColorIdx < logoColors.length - 1 ? logoColorIdx + 1 : 0
-              );
-            }}
-            to="/"
-            className="group"
-            aria-label="Home navigation"
-          >
-            <Logo
-              aria-label="Logo"
-              className={classNames(
-                "pointer-events-none mx-2 h-10 w-48 drop-shadow-sm/10 group-hover:transition-all dark:fill-white dark:drop-shadow-sm",
-                logoColors[logoColorIdx]
-              )}
-            />
-          </NavLink>
-        </span>
-        <nav className="mx-2">
-          <ul className="flex items-center gap-2 align-middle">
-            <LinkItem className="border-slate-500" to="/">
-              <span>Home</span>
-            </LinkItem>
-            <LinkItem className="border-green-500" to="/about">
-              About
-            </LinkItem>
-            <LinkItem className="border-cyan-500" to="/resume">
-              Resume
-            </LinkItem>
-            <LinkItem className="border-fuchsia-500" to="/portfolio">
-              Portfolio
-            </LinkItem>
-            <LinkItem className="border-lime-500" to="/contact">
-              Contact
-            </LinkItem>
-            <li>
-              <ModeToggle />
-            </li>
-          </ul>
-        </nav>
-      </header>
+      <footer className="glass m-2 flex items-center justify-between rounded-xl p-1 px-3 drop-shadow-xl">
+        <div>left side of the footer</div>
+        <div>right side of the footer</div>
+      </footer>
     </div>
   );
 };
--- frontend/src/pages/Root.tsx
+++ frontend/src/pages/Root.tsx
@@ -8,10 +8,9 @@ export const Root = () => {
 
   return (
     <div className="relative flex min-h-screen flex-col">
-      <Header className="absolute z-20 w-full" />
-      <main className="flex-1 bg-blue-500 pt-16">
+      <Header className="fixed top-0 left-0 z-20 w-full" />
+      <main className="flex-1 pt-16">
         <Outlet />
-        <div className="absolute h-96 w-96 bg-amber-200"></div>
       </main>
       <Footer className="" />
     </div>
