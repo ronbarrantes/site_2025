fabecc389f2453a1ea65d038e2f9dfa603f73bbc
Ron Barrantes <ronbarrantes@gmail.com>
2025-06-07 00:24:52 -0400

    Update the darkmode button

--- frontend/src/components/mode-toggle.tsx
+++ frontend/src/components/mode-toggle.tsx
@@ -14,14 +14,18 @@ export function ModeToggle() {
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
+      <DropdownMenuTrigger className="h-fit w-fit p-1" asChild>
+        <Button
+          variant="outline"
+          size="icon"
+          className="hover:bg-slate-100 hover:text-orange-500 dark:hover:bg-slate-900 dark:hover:text-cyan-300"
+        >
           <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
+      <DropdownMenuContent align="end" className="glass">
         <DropdownMenuItem onClick={() => setTheme("light")}>
           Light
         </DropdownMenuItem>
