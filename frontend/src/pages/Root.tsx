080615f507bf7e9b751fa5c61f2589d1152d943a
Ron Barrantes <ronbarrantes@gmail.com>
2025-06-08 20:16:45 -0400

    Modify size

--- frontend/src/pages/Root.tsx
+++ frontend/src/pages/Root.tsx
@@ -9,7 +9,7 @@ export const Root = () => {
   return (
     <div className="relative flex min-h-screen flex-col">
       <Header className="fixed top-0 left-0 z-20 w-full" />
-      <main className="container flex-1 border border-green-300 px-2 py-16">
+      <main className="container mx-auto flex-1 px-2 py-16">
         <Outlet />
       </main>
       <Footer className="fixed bottom-0 left-0 z-20 w-full" />
