106e85e174d17788fd8ec666508a866af90e382d
Ron Barrantes <ronbarrantes@gmail.com>
2025-06-08 22:10:29 -0400

    Update root

--- frontend/src/pages/Root.tsx
+++ frontend/src/pages/Root.tsx
@@ -4,12 +4,10 @@ import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
 
 export const Root = () => {
-  // basic layout
-
   return (
     <div className="relative flex min-h-screen flex-col">
       <Header className="fixed top-0 left-0 z-20 w-full" />
-      <main className="container mx-auto flex-1 px-2 py-16">
+      <main className="container mx-auto flex-1 px-2 pt-16 pb-8">
         <Outlet />
       </main>
       <Footer className="fixed bottom-0 left-0 z-20 w-full" />
