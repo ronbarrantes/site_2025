aa08c6a5f5702fca75927b081906ed1b7ea4342b
Ron Barrantes <ronbarrantes@gmail.com>
2025-06-09 20:42:14 -0400

    Update the dark mode button

--- frontend/src/components/mode-toggle.tsx
+++ frontend/src/components/mode-toggle.tsx
@@ -18,7 +18,7 @@ export function ModeToggle() {
         <Button
           variant="outline"
           size="icon"
-          className="hover:bg-slate-100 hover:text-orange-500 dark:hover:bg-slate-900 dark:hover:text-cyan-300"
+          className="bg-slate-100/10 hover:bg-slate-100 hover:text-orange-500 dark:hover:bg-slate-900/60 dark:hover:text-cyan-300"
         >
           <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
