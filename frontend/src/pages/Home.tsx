c153973684848b1a9fb95049abbede481ee3c534
Ron Barrantes <ronbarrantes@gmail.com>
2025-06-09 23:56:10 -0400

    Fix sizing

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -23,25 +23,22 @@ export const Home = () => {
   const isLoading = data.now.get.isLoading;
 
   return (
-    <div className="spacer-variable block items-center justify-between gap-5 overflow-y-scroll md:flex md:overflow-hidden">
-      <div className="mb-5 h-fit w-full border-2 border-green-500 md:mb-0 md:w-1/3">
+    <div className="block h-screen items-center justify-between gap-5 overflow-y-scroll border border-red-500 pt-16 md:flex md:overflow-hidden">
+      <div className="mb-0 h-fit w-full border-2 border-green-500 md:mb-10 md:w-1/3">
         <h1 className="mb-4">Welcome to my site</h1>
         <span className="text-2xl">I'm glad you found it</span>
       </div>
-      <div
-        className={clx(
-          "spacer h-screen md:w-2/3 md:overflow-hidden md:overflow-y-scroll lg:w-7/12"
-        )}
-      >
+      <div className="h-screen md:w-2/3 md:overflow-hidden md:overflow-y-scroll md:pt-8 lg:w-7/12">
         <h2 className="mb-4">What I've been up to:</h2>
-
         {isLoading ? (
           <span>LOADING ....</span>
         ) : (
-          <ul className="flex flex-col gap-5">
+          <ul className="flex flex-col gap-5 pb-8 md:pt-8 md:pb-16">
             {nowData.map((item) => (
               <NowItem key={item.id}>
-                <h3 className="text-2xl">{item.title}</h3>
+                <h3 className="text-2xl">
+                  {item.id} {item.title}
+                </h3>
                 <p>{item.desc}</p>
                 <span>{formatDate(item.created_at)}</span>
               </NowItem>
