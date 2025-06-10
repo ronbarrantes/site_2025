7a67a4c45a08bc9d9373eb89cb7d1804e58de47e
Ron Barrantes <ronbarrantes@gmail.com>
2025-06-09 22:59:46 -0400

    Stuff

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -23,14 +23,14 @@ export const Home = () => {
   const isLoading = data.now.get.isLoading;
 
   return (
-    <div className="block items-center justify-between gap-5 overflow-hidden md:flex">
+    <div className="spacer-variable block items-center justify-between gap-5 overflow-y-scroll md:flex md:overflow-hidden">
       <div className="mb-5 h-fit w-full border-2 border-green-500 md:mb-0 md:w-1/3">
         <h1 className="mb-4">Welcome to my site</h1>
         <span className="text-2xl">I'm glad you found it</span>
       </div>
       <div
         className={clx(
-          "spacer h-screen overflow-hidden overflow-y-scroll md:w-2/3 lg:w-7/12"
+          "spacer h-screen md:w-2/3 md:overflow-hidden md:overflow-y-scroll lg:w-7/12"
         )}
       >
         <h2 className="mb-4">What I've been up to:</h2>
