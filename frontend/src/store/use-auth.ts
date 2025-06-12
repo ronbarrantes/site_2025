cd2944c99ede287bc9fca45dd5eacdb1f270bd0b
Ron Barrantes <ronbarrantes@gmail.com>
2025-06-12 18:06:18 -0400

    Add is auth

---
+++ frontend/src/store/use-auth.ts
@@ -0,0 +1,26 @@
+import { create } from "zustand";
+import { devtools } from "zustand/middleware";
+
+type AuthState = {
+  isAuth: boolean;
+  editable: boolean;
+};
+
+type AuthActions = {
+  setIsAuth: (isAuth: boolean) => void;
+  setEditable: (editable: boolean) => void;
+};
+
+export type AuthStore = AuthActions & AuthState;
+
+export const useAuthStore = create<AuthStore>()(
+  devtools(
+    (set) => ({
+      isAuth: false,
+      editable: false,
+      setIsAuth: (isAuth) => set({ isAuth }),
+      setEditable: (editable) => set({ editable }),
+    }),
+    { name: "AUTH_STORE" }
+  )
+);
