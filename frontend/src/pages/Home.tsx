6bfa8fbee0081385b4c75d25d7d249231bc2c8e1
Ron Barrantes <ronbarrantes@gmail.com>
2025-06-13 16:27:55 -0400

    Modify the login stuff

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,18 @@
 import clx from "classnames";
 
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 import { useRoutes } from "@/hooks/use-api";
 import { formatDate } from "@/utils/time";
 
@@ -16,6 +29,40 @@ const NowItem = ({
     </li>
   );
 };
+
+export function SheetDemo() {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button variant="outline">Open</Button>
+      </SheetTrigger>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>Edit profile</SheetTitle>
+          <SheetDescription>
+            Make changes to your profile here. Click save when you&apos;re done.
+          </SheetDescription>
+        </SheetHeader>
+        <div className="grid flex-1 auto-rows-min gap-6 px-4">
+          <div className="grid gap-3">
+            <Label htmlFor="sheet-demo-name">Name</Label>
+            <Input id="sheet-demo-name" defaultValue="Pedro Duarte" />
+          </div>
+          <div className="grid gap-3">
+            <Label htmlFor="sheet-demo-username">Username</Label>
+            <Input id="sheet-demo-username" defaultValue="@peduarte" />
+          </div>
+        </div>
+        <SheetFooter>
+          <Button type="submit">Save changes</Button>
+          <SheetClose asChild>
+            <Button variant="outline">Close</Button>
+          </SheetClose>
+        </SheetFooter>
+      </SheetContent>
+    </Sheet>
+  );
+}
 
 export const Home = () => {
   const { data } = useRoutes();
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,7 +1,203 @@
+import { useEffect } from "react";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { loginApi, useAuthStatus, useRoutes } from "@/hooks/use-api";
+import { useAuthStore } from "@/store/use-auth";
+import { tryCatch } from "@/utils/try-catch";
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+  password: z.string().min(2, {
+    message: "Password must be at least 2 characters.",
+  }),
+});
+
+const nowSchema = z.object({
+  title: z.string().min(2, {
+    message: "Title least 2 characters.",
+  }),
+  desc: z.string().min(2, {
+    message: "Description must be at least 2 characters.",
+  }),
+});
+
+export function AddNow() {
+  // const { setIsAuth } = useAuthStore();
+  const { data } = useRoutes();
+  const form = useForm<z.infer<typeof nowSchema>>({
+    resolver: zodResolver(nowSchema),
+    defaultValues: {
+      title: "",
+      desc: "",
+    },
+  });
+
+  async function onSubmit(values: z.infer<typeof nowSchema>) {
+    const body = {
+      title: values.title,
+      desc: values.desc,
+    };
+
+    await data.now.post.mutateAsync({
+      body,
+    });
+
+    form.reset();
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Title</FormLabel>
+              <FormControl>
+                <Input placeholder="title..." {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="desc"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Description</FormLabel>
+              <FormControl>
+                <Input placeholder="description..." {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+}
+
+export function LoginForm() {
+  const { setIsAuth } = useAuthStore();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    const { data, error } = await tryCatch(
+      loginApi.login({
+        username: values.username,
+        password: values.password,
+      })
+    );
+
+    if (error) {
+      console.error(error);
+      toast.error(`There was a problem: ${error}`);
+      // form.reset();
+      return;
+    }
+
+    console.log("sucess");
+
+    console.info("data", data.data);
+    setIsAuth(true);
+    form.reset();
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="user..." {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input type="password" placeholder="pass..." {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+}
+
+const LogOutButton = () => {
+  const { setIsAuth } = useAuthStore();
+  return (
+    <Button
+      onClick={async () => {
+        const { data, error } = await tryCatch(loginApi.logout());
+        if (error) {
+          console.error(error);
+          toast.error(`There was a problem: ${error}`);
+          return;
+        }
+
+        setIsAuth(false);
+        console.info(data.data);
+      }}
+    >
+      Logout
+    </Button>
+  );
+};
+
 export const Login = () => {
+  const { isAuth, setIsAuth } = useAuthStore();
+  const authStatus = useAuthStatus();
+
+  const data = authStatus.me.get.data;
+  const error = authStatus.me.get.error;
+
+  if (error) console.warn(error);
+  if (data) setIsAuth(true);
+
   return (
     <div>
-      <div>Login goes here</div>
+      {isAuth ? <LogOutButton /> : <LoginForm />}
+      {isAuth && <AddNow />}
     </div>
   );
 };
