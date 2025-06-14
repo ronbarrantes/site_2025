f7e62f0f917531a75fba3a82cc48940c95bf8bcc
Ron Barrantes <ronbarrantes@gmail.com>
2025-06-14 14:57:48 -0400

    Make resume nicer

--- frontend/src/pages/Resume.tsx
+++ frontend/src/pages/Resume.tsx
@@ -17,39 +17,44 @@ const WItem = ({
   return (
     <div
       className={classNames(
-        "glass flex flex-col gap-2 rounded-3xl border border-slate-950/5 px-5 py-3 dark:border-white/10",
+        "glass flex flex-col gap-5 rounded-3xl border border-slate-950/5 px-5 py-3 dark:border-white/10",
         className
       )}
     >
-      <h2 className="font-semibold">{jobTitle}</h2>
-      <div className="flex gap-2">
-        {url ? (
-          <a
-            href={url}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="font-semibold text-fuchsia-700 underline hover:no-underline dark:text-fuchsia-400"
-          >
-            {employer}
-          </a>
-        ) : (
-          <span>{employer}</span>
-        )}
-        <span className="">::</span>
-        <span>{`${startDate} | ${endDate ? endDate : "Present"}`}</span>
+      <div className="flex flex-col gap-2">
+        <h2 className="font-semibold">{jobTitle}</h2>
+        <div className="flex gap-2">
+          {url ? (
+            <a
+              href={url}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="font-semibold text-fuchsia-700 underline hover:no-underline dark:text-fuchsia-400"
+            >
+              {employer}
+            </a>
+          ) : (
+            <span>{employer}</span>
+          )}
+          <span className="">::</span>
+          <span>{`${startDate} | ${endDate ? endDate : "Present"}`}</span>
+        </div>
       </div>
-
-      {description.map((item, idx) => (
-        <p key={`${startDate}-${idx}`}>{item}</p>
-      ))}
-      <span>Tools used:</span>
-      <ul className="flex gap-3">
-        {tools.map((item, idx) => (
-          <li key={`${startDate}-${item}-${idx}`}>
-            <Icon tooltip name={item} />
-          </li>
+      <div className="flex flex-1 flex-col gap-2">
+        {description.map((item, idx) => (
+          <p key={`${startDate}-${idx}`}>{item}</p>
         ))}
-      </ul>
+      </div>
+      <div className="flex flex-col gap-2">
+        <span>Tools used:</span>
+        <ul className="ml-2 flex gap-3">
+          {tools.map((item, idx) => (
+            <li key={`${startDate}-${item}-${idx}`}>
+              <Icon tooltip name={item} />
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
@@ -57,11 +62,13 @@ const WItem = ({
 export const Resume = () => {
   return (
     <div className="flex h-screen flex-col gap-3 overflow-hidden overflow-y-scroll pt-18 pb-10">
-      <h1>{workHistoryText.title}</h1>
-      <p>{workHistoryText.description}</p>
+      <div className="mx-8 flex flex-col gap-3">
+        <h1>{workHistoryText.title}</h1>
+        <p>{workHistoryText.description}</p>
+      </div>
       <div>
         {/*        <ul className="flex w-full flex-col flex-wrap gap-8 border border-red-600 md:flex-row md:gap-0">*/}
-        <ul className="flex flex-col md:flex-row md:flex-wrap">
+        <ul className="flex flex-col gap-3 md:flex-row md:flex-wrap md:gap-0">
           {experienceItems.map((item, idx) => {
             return (
               <li
